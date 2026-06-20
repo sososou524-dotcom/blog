d0ab11095cfd49c7c986f4a1696b4f9a250b0836
Souki <sawaganiuniv@gmail.com>
2026-06-20 21:05:02 +0900

    output staticに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,10 @@
 
 /** @type {import('astro').AstroUserConfig} */
 export default {
+  output: "static",
+};// @ts-check
+
+/** @type {import('astro').AstroUserConfig} */
+export default {
   output: "server",
 };
